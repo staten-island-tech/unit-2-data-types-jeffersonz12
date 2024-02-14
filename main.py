d91d4408ea334c495e00d97e9b01f972f22218af
jeffersonz12 <158225349+jeffersonz12@users.noreply.github.com>
2024-02-14 12:18:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,14 @@ for i in factors:
 
 
 #greatest common factor
-firstnum = float(input("Number1: "))
-secondnum = float(input("Number2: "))
-print(f"GCF: {gcd(int(firstnum), int(secondnum))}")
+num1 = float(input(""))
+num2 = float(input(""))
+gcf = 1
+if num1 < num2:
+    smaller = num1
+else:
+    smaller = num2
+for i in range(1, int(smaller+1)):
+    if num1%i == 0 and num2%i == 0:
+        gcf = i
+print(gcf)
